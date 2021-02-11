5af29739093683dd9220dcfc1b539d45b3c47a28
Maddax Lallatin <65627323+papamaddax@users.noreply.github.com>
2021-02-11 17:41:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const  Discord = require('discord.js');
 
 const client = new Discord.Client();
 
+const fs = require('fs')
+
 require('dotenv').config();
 
 client.commands = new Discord.Collection();
