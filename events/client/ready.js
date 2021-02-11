d882387fbd552484219475ef0b926e92a5aa3833
Maddax Lallatin <65627323+papamaddax@users.noreply.github.com>
2021-02-11 17:36:49 -0500

    Formatting

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,4 +1,4 @@
 module.exports = (Discord, client) =>{
     console.log(`${client.user.tag} is online`);
-    client.user.setPresence({ activity: { type: 'WATCHING',name: 'AAA Server' },  status: 'dnd' });
+    client.user.setPresence({ activity: { type: 'WATCHING',name: '-help' },  status: 'dnd' });
 }
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const  Discord = require('discord.js');
 
 const client = new Discord.Client();
-require('dotenv').config();
 
-const fs = require('fs');
+require('dotenv').config();
 
 client.commands = new Discord.Collection();
 client.commands = new Discord.Collection();
@@ -12,33 +11,4 @@ client.commands = new Discord.Collection();
     require(`./handlers/${handler}`)(client, Discord);
 })
 
-
-/* client.on("message", (message) => {
-    let args = message.content.substring(prefix.length).split(" ");
-
-    switch (args[0]) {
-        case '-clock':
-            client.commands.get('clock').execute(message, args);
-            break;
-
-        case '-suggest':
-            client.commands.get('suggest').execute(message, args);
-            break;
-
-        case '-rq':
-            client.commands.get('request').exectue(message, args);
-            break;
-        case '-documents':
-            client,commadns.get('documents').execute(message, args)
-            break;
-
-
-
-
-
-    }
-
-
-})
-*/
 client.login(process.env.DISCORD_TOKEN);
