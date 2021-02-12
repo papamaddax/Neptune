214858aef72cda745663d8455753446a54c0e5f6
Maddax Lallatin <65627323+papamaddax@users.noreply.github.com>
2021-02-12 13:51:51 -0500

    Update embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -1,12 +1,15 @@
 module.exports = {
     name: "embed",
     aliases: ["aliase"],
-   async execute(client, Discord, message) {
-        const prefix = '-';
-        let args = message.content.substring(prefix.length).split(",");
+   async execute(client, Discord, message, args) {
+       let title = args[0]
+       let description = args.slice(2).join(" ")
+       let color = args[1]
         const customEmbed = new Discord.MessageEmbed
-        .setTitle(args[0])
-        .setDescription(args[1])
+        .setTitle(title)
+        .setDescription(description)
+        .setColor(color)
+        .setFooter(message.author.tag)
         message.channel.send(customEmbed)
 
 
