6633ffe634b5a30daea1dd74f03fcd54428deb27
Maddax Lallatin <65627323+papamaddax@users.noreply.github.com>
2021-02-12 13:32:48 -0500

    Create embed.js

---
+++ commands/embed.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: "embed",
+    aliases: ["aliase"],
+    execute(client, Discord, message) {
+        const prefix = '-';
+        const args = message.content.slice(prefix.length).split(",");
+        const customEmbed = new Discord.MessageEmbed
+        .setTitle(args[0])
+        .setDescription(args[1])
+
+
+    },
+};
