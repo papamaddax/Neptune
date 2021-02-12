70ceb4fc9518cc8e733e487887af167a0377c93e
Maddax Lallatin <65627323+papamaddax@users.noreply.github.com>
2021-02-12 13:40:45 -0500

    Update embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -1,9 +1,11 @@
+const prefix = '-';
+
+const args = message.content.slice(prefix.length).split(",");
+
 module.exports = {
     name: "embed",
     aliases: ["aliase"],
     execute(client, Discord, message) {
-        const prefix = '-';
-        const args = message.content.slice(prefix.length).split(",");
         const customEmbed = new Discord.MessageEmbed
         .setTitle(args[0])
         .setDescription(args[1])
